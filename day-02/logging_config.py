7bbe141458fb664b935bb4d7859ab1e04a13a667
tolanny-studio <hellotolannystudio@gmail.com>
2026-08-06 12:23:21 +0100

    fix: add force to logging configuration to make it deterministic

--- day-02/logging_config.py
+++ day-02/logging_config.py
@@ -1,9 +1,12 @@
+from pathlib import Path
 import logging
+
+LOG_FILE = Path(__file__).parent / "tip_calculator.log"
+
 logging.basicConfig(
-    filename="tip_calculator.log",
-    filemode="a",
+    filename=LOG_FILE,
+    filemode="w",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    force=True,
 )
-
-logger = logging.getLogger(__name__)
--- day-02/validate.py
+++ day-02/validate.py
@@ -1,5 +1,4 @@
 import logging
-import logging_config
 
 logger = logging.getLogger(__name__)
 
